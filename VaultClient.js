0dd8c9332fc3a3cab104a313365a4732816efb31
mohsinanees <46046733+mohsinanees@users.noreply.github.com>
2019-12-15 15:46:20 +0500

    log path change

--- VaultClient.js
+++ VaultClient.js
@@ -6,7 +6,7 @@ const request = require('request')
 let logger = require('perfect-logger');
 
 // Configure Settings
-logger.setLogDirectory("./logs");
+logger.setLogDirectory("/home/dawood.ud/logs");
 logger.setLogFileName("client");
 
 // Initialize
